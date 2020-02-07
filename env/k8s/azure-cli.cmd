6790085b5f934bab9ea651ab0dbd64663480e0fe
Wilson Bo <wilsonbo@leansoftx.com>
2020-02-07 20:45:58 +0800

    update env generator scripts

--- env/k8s/azure-cli.cmd
+++ env/k8s/azure-cli.cmd
@@ -1,4 +1,4 @@
-az login
-az account set --subscription <subscriptionId>
-az group create --name k8sTestRG --location "Southeast Asia"
-az group deployment create --resource-group k8sTestRG --template-file azuredeploy.json --parameters @"C:\WilsonBoData\tmp\gits\icdps\icdps-general\env\k8s\arm\params.json"
+az login -u wilsonbo@leansoftx.com -p Croco321!
+az account set --subscription <subscription name or id>
+az group create --name ICDPS-ESA-K8S-RG --location "Southeast Asia"
+az group deployment create --resource-group ICDPS-ESA-K8S-RG --template-file "arm\azuredeploy.json" --parameters "C:\WilsonBoData\tmp\gits\icdps\icdps-general\env\k8s\arm\params.json"
